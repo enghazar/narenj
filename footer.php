0452edb2292ff7c4490061b824d583a74ddfdf9b
Hazar Kaysi <enghazar@gmail.com>
2019-06-10 23:20:36 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-
-<footer>
-   
-    <div class="mentions"><a id="mentions" href="mentions_legales.php">Mentions légales</a></div>  
-    <div class="reseaux">Suivez-nous <a href="https://www.facebook.com/narenj.fr/" target="_blank"><img src="img/facebook.png" width="30px" alt="Facebook"></a>
-        <a href="https://www.instagram.com/narenj.fr/?hl=fr" target="_blank"><img src="img/instagram.jpg" width="30px" alt="Instagram"></a></div>
-    <div><p>Site concu par Hazar KAYSI</p></div>
-
-</footer>
